2ee5fbafd6492c6acf44d3dde1fc8b541d65e3d3
Rajat <saxena1975sanjeev@gmail.com>
2021-05-18 21:00:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
 import React, {useState} from "react";
 import './App.css';
+import { jsPDF } from "jspdf";
+// import { Base64 } from 'js-base64';
+// import {html2canvas} from 'html2canvas';
+
+// IMPORTING FONTS
+import Caveat from './assets/fonts/Caveat';
+import ArchitectsDaughter from './assets/fonts/ArchitectsDaughter';
+import DancingScript from './assets/fonts/DancingScript';
+import GreatVibes from './assets/fonts/GreatVibes';
+import Satisfy from './assets/fonts/Satisfy';
+
 
 //IMPORTING IMAGES
-import page1 from './assets/img/page-1.png'
+import paper1 from './assets/img/paper-1.png';
+import paper2 from './assets/img/paper-2.png';
+import paper3 from './assets/img/paper-3.png';
+import paper4 from './assets/img/paper-4.png';
+
 
 function App() {
 
+  // HEX TO RGB CONVERTER
+  var colorHandeler = (hex) => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  // PDF GENERATOR
+  const getPdf = () => {
+    var doc = new jsPDF();
+
+    //SETTING PAGE IN PDF
+    switch (paper?paper:"paper1") {
+    case "paper1":
+      doc.addImage(paper1, 0, 0, 210, 297);
+      break;
+    case "paper2":
+      doc.addImage(paper2, 0, 0, 210, 297);
+      break;
+    case "paper3":
+      doc.addImage(paper3, 0, 0, 210, 297);
+      break;
+    case "paper4":
+      doc.addImage(paper4, 0, 0, 210, 297);
+      break;
+      default:
+
+    }
+
+    //SETTING FONT COLOR IN PDF
+    var colorObj = colorHandeler(color?color:"#000000")
+    doc.setTextColor(colorObj.r, colorObj.g, colorObj.b);
+
+    //SETTING FONT IN PDF
+    switch (family?family:"Satisfy") {
+      case "Architects Daughter":
+        doc.addFileToVFS("MyFont.ttf", ArchitectsDaughter);
+        break;
+      case "Caveat":
+        doc.addFileToVFS("MyFont.ttf", Caveat);
+        break;
+      case "Dancing Script":
+        doc.addFileToVFS("MyFont.ttf", DancingScript);
+        break;
+      case "Great Vibes":
+        doc.addFileToVFS("MyFont.ttf", GreatVibes);
+        break;
+      case "Satisfy":
+        doc.addFileToVFS("MyFont.ttf", Satisfy);
+        break;
+      default:
+
+    }
+
+    doc.addFont("MyFont.ttf", "MyFont", "normal");
+    doc.setFont("MyFont");
+    doc.text(content, x, y);
+    doc.save("a4.pdf");
+    console.log(x);
+  };
+
+  //GET PAPER
+  const getPaper = () => {
+    console.log(paper);
+  }
+
+  // HOOKS
   const [content, setContent] = useState("");
   const [family, setFamily] = useState("");
   const [size, setSize] = useState("");
   const [color, setColor] = useState("");
+  const [paper, setPaper] = useState("");
   const [x, setX] = useState("");
   const [y, setY] = useState("");
+
 
   return (
     <div className="App container">
       <div className="form-group">
         <div className="row">
           <div className="col-sm-6">
-            <h1>My Homework</h1>
+            <h1>Hi Homework</h1>
           </div>
           <div className="row col-sm-6">
             <label className="col-2" htmlFor="font-select">Font:</label>
@@ -57,18 +144,21 @@ function App() {
             <label className="col-2" htmlFor="paper-select">Paper:</label>
             <select
               className="form-control col-3"
-              id="paper-select">
-              <option>Plane Paper</option>
-              <option>Option2</option>
+              id="paper-select"
+              onChange={(e)=>setPaper(e.target.value)}>
+              <option value="paper1">Paper-1</option>
+              <option value="paper2">Paper-2</option>
+              <option value="paper3">Paper-3</option>
+              <option value="paper4">Paper-4</option>
             </select>
             <label className="col-1" htmlFor="paper-select">X:</label>
             <input
               className="form-control col-2"
               type="number"
-              min="0"
+              min="3"
               value={x}
               onChange={(e)=>setX(e.target.value)}
-              placeholder="5" />
+              placeholder="3" />
             <label className="col-1" htmlFor="paper-select">Y:</label>
             <input
               className="form-control col-2"
@@ -76,7 +166,7 @@ function App() {
               min="0"
               value={y}
               onChange={(e)=>setY(e.target.value)}
-              placeholder="15" />
+              placeholder="0" />
           </div>
         </div>
 
@@ -94,7 +184,11 @@ function App() {
             </textarea>
           </div>
           <div className="col-sm-6" id="previewArea">
-            <img id="preview-img" src={page1} alt="" />
+            <img
+              id="preview-img"
+              src={paper=="paper1"?paper1:paper=="paper2"?paper2:paper=="paper3"?paper3:paper4}
+              alt=""
+            />
             <p
               id="content"
               style={{
@@ -113,7 +207,7 @@ function App() {
 
         <div className="row">
           <div className="col-10"></div>
-          <button className="btn btn-primary col-2">Download</button>
+          <button className="btn btn-primary col-2" onClick={getPdf}>Download</button>
         </div>
       </div>
     </div>
